0b6855a4e9bdf2a0089006c61e3846a74cf9196d
Daniel Carlson <dancarl758@gmail.com>
2019-11-06 00:24:17 +0100

    Initial commit

---
+++ projects/magic-strings/src/lib/magic-strings.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nk-magic-strings',
+  template: `
+    <p>
+      magic-strings works!
+    </p>
+  `,
+  styles: []
+})
+export class MagicStringsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
